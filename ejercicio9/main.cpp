918f087ba96ab5c65096cd3e56151bcbf1628042
Team-MMMBCI <55298738+Team-MMMBCI@users.noreply.github.com>
2019-09-14 18:29:23 -0500

    Update main.cpp

--- ejercicio9/main.cpp
+++ ejercicio9/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include "tipos.h"
-#include "MyFunc.h"
 using namespace std;
-int main() {
-    l_enteros segundos;
-    enteros horas, min, seg;
 
-    do {
-        cout << "Segundos: ";
-        cin >> segundos;
-    } while (segundos <1);
+int main(){
 
-    // ToDO
-    return 0;
+  int entrada, horas, minutos, segundos;
+
+  cout << "Ingrese los segundos: \n"; cin >> entrada;
+
+  horas = (entrada/3600);
+  minutos = ((entrada - horas * 3600) / 60);
+  segundos = entrada - (horas * 3600 + minutos *60);
+  
+  cout << "Son " << horas << " horas con "<< minutos << " minuto(s) y " << segundos << " segundos ";
+  return 0;
 }
