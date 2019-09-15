160ceac0af69b91f0fa17794fd19782ef7b54664
Team-MMMBCI <55298738+Team-MMMBCI@users.noreply.github.com>
2019-09-14 21:18:08 -0500

    Update main.cpp

--- ejercicio7/main.cpp
+++ ejercicio7/main.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 int main(){
   int numero, i;
-  
+  a:
   cout << "Ingrese la cantidad de asteriscos: \n";
-  cin >> numero;
+  cin >> numero;  
+
+  if (numero<10 || numero >30){
+    cout << "Cantidad fuera de rango. \n";
+    cout << "\n";
+
+    goto a;
+  }
 
   for (i=1; i<=numero; i++){
     cout << "*";
