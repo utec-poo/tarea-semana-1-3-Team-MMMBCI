503f1d5d87a4a6369e9ec4c56819db1a7a7608a2
Team-MMMBCI <55298738+Team-MMMBCI@users.noreply.github.com>
2019-09-19 07:10:58 -0500

    Update main.cpp

--- ejercicio10/main.cpp
+++ ejercicio10/main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
+typedef long int tipo_Entero;
 
-entero numero;
-caracter crt;
+tipo_Entero LeeNumero();
+tipo_Entero NumeroInvertido(tipo_Entero num);
 
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
+tipo_Entero LeeNumero(){
+  tipo_Entero n;
+  do{
+    cout <<"Numero de por lo menos 7 digitos: ";
+    cin>>n;
+  }while(n<1000000);
+  return n;
+}
 
-// ToDO
-// implemente el algoritmo.
+tipo_Entero NumeroInvertido(tipo_Entero num){
+
+  tipo_Entero numeroAlReves, digito;
+
+  numeroAlReves=0;
+  while(num>0)
+  {
+    digito = num%10;
+    numeroAlReves = numeroAlReves*10 + digito;
+    num/=10;
+  }
+return numeroAlReves;
+}
+
+int main(){
+  tipo_Entero numero;
+
+  numero=LeeNumero();
+  if(numero == NumeroInvertido(numero))
+    cout<<"Es capicua";
+  else
+    cout<<"No es capicua";
   return 0;
 }
